3eeb3fd7fc642d46846595ceabde9d17de95a4bc
Endhong0929 <148163235+Endhong0929@users.noreply.github.com>
2026-06-25 16:52:38 +0800

    connection parameters

--- netconf_user_banner.py
+++ netconf_user_banner.py
@@ -3,3 +3,9 @@ from ncclient.operations import RPCError
 import xmltodict
 import json
 import os
+
+#Connection parameters
+HOST = os.getenv("DEVICE_HOST", "192.168.56.101")
+PORT = int(os.getenv("DEVICE_PORT", "830"))
+USER = os.getenv("DEVICE_USER", "cisco")
+PASS = os.getenv("DEVICE_PASS", "cisco")
