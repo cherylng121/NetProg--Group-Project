accb7fbe547c8763c210cf1c521e8caf5efdf8da
LEONARD LEE <leonardlee165@gmail.com>
2026-06-28 23:11:26 +0800

    Add Hostname , Date and Time
    hostname, date and time info the linux system

--- linux_info.py
+++ linux_info.py
@@ -1,1 +1,8 @@
+import subprocess
 
+def run():
+    print("--- LINUX SYSTEM INFORMATION ---")
+    print("\n[Hostname]")
+    subprocess.run(["hostname"])
+    print("\n[Current Date and Time]")
+    subprocess.run(["date"])
